19b4c5d32cbfa509d2ca843562b91877739458bb
wuqiao <qiao.wu@pingcl.com>
2020-09-27 10:10:48 +0800

    feat: 增加提交限制

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,3 @@
-/*
-	Created by qiao.wu@pingcl.com on 2020/9/24.
-*/
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
@@ -11,8 +8,12 @@ module.exports = {
         "feat", //新特性
         "fix", //修复问题
         "docs", //文档修改
+        "perf", // 提升性能的修改
         "style", // 代码格式修改, 注意不是 css 修改
-        "chore" //其他修改, 比如构建流程, 依赖管理.
+        "refactor", //代码重构
+        "test", // 测试用例修改
+        "chore", //其他修改, 比如构建流程, 依赖管理.
+        "revert", // 代码回滚
       ],
     ],
     "subject-full-stop": [0, "never"], // subject结尾不能有句号
